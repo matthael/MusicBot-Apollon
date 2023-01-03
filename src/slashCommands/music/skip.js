6ac52f4cb51b3852103a470c9c1334d271325977
Big anão <103335886+Biganao@users.noreply.github.com>
2023-01-03 14:36:38 -0300

    Add files via upload

---
+++ src/slashCommands/music/skip.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'skip',
+  description: 'Skips the current track',
+  inVc: true,
+  sameVc: true,
+  player: true,
+  run: (client, interaction) => {
+    const player = client.poru.players.get(interaction.guild.id);
+    if (interaction.user.id !== player.currentTrack.info.requester.id)
+      return interaction.reply({ content: `You are not allowed to use this command now as the song is played by another user`, ephemeral: true });
+    player.stop();
+
+    const embed = new EmbedBuilder()
+      .setColor('#f7bd3f')
+      .setDescription('Skipped the current track');
+
+    interaction.reply({ embeds: [embed] });
+  },
+};
